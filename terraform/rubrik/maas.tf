a0e845a884ce8535c0de26e5360a5e407b5a869b
Nicholas Morey <nicholas@morey.tech>
2024-03-10 15:20:43 +0000

    feat: add maas vlans
    Signed-off-by: Nicholas Morey <nicholas@morey.tech>

--- terraform/rubrik/maas.tf
+++ terraform/rubrik/maas.tf
@@ -2,6 +2,31 @@ resource "maas_fabric" "home_morey_tech" {
   name = "home-morey-tech"
 }
 
+resource "maas_vlan" "lan" {
+  fabric = maas_fabric.home_morey_tech.id
+  vid    = 1
+  name   = "lan"
+}
+
+resource "maas_vlan" "lab" {
+  fabric = maas_fabric.home_morey_tech.id
+  vid    = 3
+  # name   = "lab"
+  name   = "untagged"
+}
+
+resource "maas_vlan" "bmc" {
+  fabric = maas_fabric.home_morey_tech.id
+  vid    = 6
+  name   = "bmc"
+}
+
+resource "maas_vlan" "k8s_hosts" {
+  fabric = maas_fabric.home_morey_tech.id
+  vid    = 8
+  name   = "k8s-hosts"
+}
+
 resource "maas_machine" "rubrik_c" {
   power_type = "ipmi"
   power_parameters = jsonencode({
