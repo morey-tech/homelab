87bdb919c6a9e41a3d78f1c5c845db7f0e4a1aa7
Nicholas Morey <nicholas@morey.tech>
2024-03-10 15:43:30 +0000

    feat: add maas subnets
    Signed-off-by: Nicholas Morey <nicholas@morey.tech>

--- terraform/rubrik/maas.tf
+++ terraform/rubrik/maas.tf
@@ -27,6 +27,68 @@ resource "maas_vlan" "k8s_hosts" {
   name   = "k8s-hosts"
 }
 
+resource "maas_subnet" "lan" {
+  cidr       = "192.168.1.0/24"
+  fabric     = maas_fabric.home_morey_tech.id
+  vlan       = maas_vlan.lan.vid
+  name       = "lan"
+  gateway_ip = "192.168.1.1"
+  ip_ranges {
+    type     = "reserved"
+    start_ip = "192.168.1.100"
+    end_ip   = "192.168.1.200"
+    comment  = "pfSense DHCP"
+  }
+}
+
+resource "maas_subnet" "lab" {
+  cidr       = "192.168.3.0/24"
+  fabric     = maas_fabric.home_morey_tech.id
+  vlan       = maas_vlan.lab.vid
+  name       = "lab"
+  gateway_ip = "192.168.3.1"
+  ip_ranges {
+    type     = "reserved"
+    start_ip = "192.168.3.1"
+    end_ip   = "192.168.3.10"
+    comment  = "static hardware"
+  }
+  ip_ranges {
+    type     = "reserved"
+    start_ip = "192.168.3.100"
+    end_ip   = "192.168.3.199"
+    comment  = "proxmox"
+  }
+  ip_ranges {
+    type     = "reserved"
+    start_ip = "192.168.3.200"
+    end_ip   = "192.168.3.250"
+    comment  = "lab bmc"
+  }
+}
+
+resource "maas_subnet" "bmc" {
+  cidr       = "192.168.6.0/24"
+  fabric     = maas_fabric.home_morey_tech.id
+  vlan       = maas_vlan.bmc.vid
+  name       = "bmc"
+  gateway_ip = "192.168.6.1"
+}
+
+resource "maas_subnet" "k8s_hosts" {
+  cidr       = "192.168.8.0/24"
+  fabric     = maas_fabric.home_morey_tech.id
+  vlan       = maas_vlan.k8s_hosts.vid
+  name       = "k8s-hosts"
+  gateway_ip = "192.168.8.1"
+  ip_ranges {
+    type     = "dynamic"
+    start_ip = "192.168.8.200"
+    end_ip   = "192.168.8.250"
+    comment  = "Dynamic"
+  }
+}
+
 resource "maas_machine" "rubrik_c" {
   power_type = "ipmi"
   power_parameters = jsonencode({
