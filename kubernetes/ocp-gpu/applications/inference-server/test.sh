1214426429f1a51db6a39d92b57df68e38ac2508
morey-tech <nicholas@morey.tech>
2025-11-16 23:46:08 +0000

    feat: add nfd instance

---
+++ kubernetes/ocp-gpu/applications/inference-server/test.sh
@@ -0,0 +1,12 @@
+# Call the server using curl:
+curl -X POST "http://interence-server-inference-server.apps.ocp-gpu.rh-lab.morey.tech/v1/chat/completions" \
+	-H "Content-Type: application/json" \
+	--data '{
+		"model": "Qwen3-Coder-30B-A3B-Instruct-Q4_K_M.gguf",
+		"messages": [
+			{
+				"role": "user",
+				"content": "What is the capital of France?"
+			}
+		]
+	}'
