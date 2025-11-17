33383d72c7870d906b7fe2a7d4a7bbcbc32287c7
morey-tech <nicholas@morey.tech>
2025-11-17 00:15:54 +0000

    fix: https for test route

--- kubernetes/ocp-gpu/applications/inference-server/test.sh
+++ kubernetes/ocp-gpu/applications/inference-server/test.sh
@@ -1,5 +1,5 @@
 # Call the server using curl:
-curl -X POST "http://interence-server-inference-server.apps.ocp-gpu.rh-lab.morey.tech/v1/chat/completions" \
+curl --insecure -X POST "https://interence-server-inference-server.apps.ocp-gpu.rh-lab.morey.tech/v1/chat/completions" \
 	-H "Content-Type: application/json" \
 	--data '{
 		"model": "Qwen3-Coder-30B-A3B-Instruct-Q4_K_M.gguf",
